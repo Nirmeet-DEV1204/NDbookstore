5253ba5b09110e60c23c6e2d48ef6007d59477a6
Nirmeet Pansuriya <96394155+Nirmeet-DEV1204@users.noreply.github.com>
2022-11-25 12:12:17 -0500

    Add files via upload

---
+++ NDBooks.Models/CoverType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NDBooks.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int id { get; set; }
+        [Display(Name = "CoverType Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
